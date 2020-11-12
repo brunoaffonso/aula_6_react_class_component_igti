312bd2458345d2c0aa4f41594bd64ec0e7a0a45c
Bruno Affonso Teixeira <brunoaffonso@gmail.com>
2020-11-12 12:43:03 -0300

    initial commit

---
+++ src/components/Counter/decrementButton.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class decrementButton extends Component {
+    handleButtonClick = () => {
+        this.props.onIncrement('-');
+    };
+
+    render() {
+        return (
+            <button
+                onClick={this.handleButtonClick}
+                className="waves-effect waves-light btn red darken-4"
+            >
+                -
+            </button>
+        );
+    }
+}
